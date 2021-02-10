96e8037f18b91d4bd57a0b3718399f864a49db24
kaylamuraoka <kmurs98@gmail.com>
2021-02-09 15:35:48 -1000

    adding login.js code for the client side

---
+++ client/assets/js/login.js
@@ -0,0 +1,135 @@
+$(document).ready(function () {
+  // Getting references to our form and input fields
+  const loginForm = $("form.login");
+  const emailInput = $("input#email-input");
+  const passwordInput = $("input#password-input");
+
+  // Getting references to our input validation messages
+  const emailFeedback = $("small#email-validation");
+  const passwordFeedback = $("small#pwd-validation");
+
+  // Getting references to our error alert section
+  const errorBox = $("#alert");
+  const errorMsg = $("#alert .msg");
+
+  // When the form is submitted, we validate there's an email and password entered
+  loginForm.on("submit", (event) => {
+    event.preventDefault();
+
+    const userData = {
+      email: emailInput.val().trim(),
+      password: passwordInput.val(),
+    };
+
+    // Validate that input fields meet all specified requirements
+    if (checkEmail(userData.email)) {
+      // If we have an email and password we run the loginUser function and clear the form
+      loginUser(userData);
+    } else {
+      showError(
+        errorBox,
+        errorMsg,
+        "You must fix all errors before you can proceed"
+      );
+      return;
+    }
+  });
+
+  // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
+  const loginUser = (userData) => {
+    $.ajax({
+      type: "POST",
+      url: "/api/login",
+      data: userData,
+    })
+      .then(() => {
+        succcessfulLoginAlert();
+      })
+      .catch((error) => {
+        const msg = error.responseJSON.message;
+        handleLoginErr(msg);
+      });
+  };
+
+  function handleLoginErr(msg) {
+    console.log("Error message: " + msg);
+    passwordInput.val("");
+    switch (msg) {
+      case "The password you entered is incorrect. Please try again.":
+        showError(errorBox, errorMsg, msg);
+        invalidInput(passwordInput, passwordFeedback, msg);
+        break;
+      case "No account found with that email address.":
+        invalidInput(emailInput, emailFeedback, msg);
+        invalidInput(passwordInput, passwordFeedback, msg);
+        authErrorAlert(msg, "Please create an account.", "Sign Up", "/signup");
+        break;
+      default:
+        msg = "Invalid login credentials. Please try again.";
+        showError(errorBox, errorMsg, msg);
+        invalidInput(emailInput, emailFeedback, msg);
+        invalidInput(passwordInput, passwordFeedback, msg);
+    }
+  }
+
+  emailInput.on("input", function () {
+    let email = $(this).val().trim();
+
+    if (checkEmail(email)) {
+      // is true
+      validInput($(this), emailFeedback, "Looks Good!");
+    } else {
+      invalidInput(
+        $(this),
+        emailFeedback,
+        "Enter the email address associated with your account."
+      );
+    }
+  });
+
+  // Validate password
+  passwordInput.on("input", function () {
+    const pwd = $(this).val();
+
+    if (checkPwd(pwd)) {
+      // is true
+      validInput(passwordInput, passwordFeedback, "Looks Good!");
+    } else {
+      invalidInput(
+        passwordInput,
+        passwordFeedback,
+        "Enter the password associated with your account."
+      );
+    }
+  });
+
+  // Function that alerts the user that their credentials is correect and they are being redirected to their dashboard
+  function succcessfulLoginAlert() {
+    Swal.fire({
+      title: "Logging you in",
+      icon: "success",
+      html: "<b></b> milliseconds left.",
+      timer: 1500,
+      timerProgressBar: true,
+      didOpen: () => {
+        Swal.showLoading();
+        timerInterval = setInterval(() => {
+          const content = Swal.getContent();
+          if (content) {
+            const b = content.querySelector("b");
+            if (b) {
+              b.textContent = Swal.getTimerLeft();
+            }
+          }
+        }, 100);
+      },
+      willClose: () => {
+        clearInterval(timerInterval);
+      },
+    }).then((result) => {
+      if (result.dismiss === Swal.DismissReason.timer) {
+        window.location.replace("/members");
+      }
+    });
+  }
+});
