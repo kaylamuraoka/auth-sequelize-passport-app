c6e7799e30707b008b5183eb090946fce33a337c
kaylamuraoka <kmurs98@gmail.com>
2021-02-09 15:56:56 -1000

    adding js code to the funcationality of the user-dashboard.html

---
+++ client/assets/js/user-dashboard.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  // This file just does a GET request to figure out which user is logged in
+  // and updates the HTML on the page
+  $.get("/api/user_data").then((data) => {
+    console.log(data);
+
+    const greeting = getGreeting();
+    $("#greeting").text(greeting + ", " + data.firstName + " " + data.lastName);
+
+    $(".user-email").text(data.email);
+    $(".user-phone").text(data.phone);
+  });
+});
