e73c348b43c52a1684c387526c9aea9c98ed1c44
kaylamuraoka <kmurs98@gmail.com>
2021-02-10 19:13:42 -1000

    adding a controllers folder with functions to be imported into the routes file

---
+++ controllers/auth-controller.js
@@ -0,0 +1,123 @@
+// Requiring our models, passport and our written passport.js, as well as bcrypt for when user's want to update their password.
+const db = require("../models");
+const passport = require("../config/passport");
+
+const authController = {
+  // -------------------------------------------------------------------
+  // Logout user
+  logout(req, res) {
+    req.logout();
+    res.redirect("/login");
+  },
+
+  // -------------------------------------------------------------------
+  // Login user using the passport.authenticate middleware with our local strategy.
+  login(req, res, next) {
+    passport.authenticate("local", (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.status(401).json({
+          message: info.message,
+        });
+      }
+      req.logIn(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        // User has provided valid login credentials
+        return res.status(200).json({ message: "success" });
+      });
+    })(req, res, next);
+  },
+
+  // -------------------------------------------------------------------
+  // Signup/create a new user
+  // Passwords will be auto hashed and stored securely thanks to how we configured our Sequelize User Model.
+  signup(req, res) {
+    db.User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+      password: req.body.password,
+    })
+      .then(() => {
+        // user is created successfully, proceed to log the user in
+        res.redirect(307, "/api/login");
+      })
+      .catch((error) => {
+        // otherwise send back an error
+        res.status(401).json(error);
+      });
+  },
+
+  // -------------------------------------------------------------------
+  // Function for getting some data about our user to be used by the client side
+  getUserData(req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.status(404).json({});
+    }
+
+    // Otherwise send back the user's email and id
+    // Sending back a password, even a hashed password, isn't a good idea
+    db.User.findOne({
+      where: { id: req.user.id },
+      attributes: { exclude: ["password"] },
+    })
+      .then((dbUser) => {
+        res.status(200).json(dbUser);
+      })
+      .catch((error) => {
+        res.status(401).json(error);
+      });
+  },
+
+  // -------------------------------------------------------------------
+  // Function for updating a user's information.
+  updateUser(req, res) {
+    if (!req.user) {
+      return;
+    }
+
+    db.User.update(
+      {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        phone: req.body.phone,
+      },
+      {
+        where: {
+          id: req.user.id,
+        },
+      }
+    )
+      .then((dbUser) => {
+        res.status(200).json(dbUser);
+      })
+      .catch((error) => {
+        res.status(401).json(error);
+      });
+  },
+
+  // -------------------------------------------------------------------
+  // Function for deleting a user
+  deleteUser(req, res) {
+    db.User.destroy({
+      where: {
+        id: req.user.id,
+      },
+    })
+      .then((dbUser) => {
+        res.status(200).json(dbUser);
+      })
+      .catch((error) => {
+        res.status(401).json(error);
+      });
+  },
+};
+
+module.exports = authController;
--- routes/auth-api-routes.js
+++ routes/auth-api-routes.js
@@ -1,121 +1,24 @@
-// Requiring our models, passport and our written passport.js, as well as bcrypt for when user's want to update their password.
-const db = require("../models");
-const passport = require("../config/passport");
-const bcrypt = require("bcryptjs");
+// Require controller modules
+const authController = require("../controllers/auth-controller");
 
 module.exports = (app) => {
-  // POST Route that signs a user in, using the passport.authenticate middleware with our local strategy.
-  app.post("/api/login", (req, res, next) => {
-    passport.authenticate("local", (err, user, info) => {
-      if (err) {
-        return next(err);
-      }
-      if (!user) {
-        return res.status(401).json({
-          message: info.message,
-        });
-      }
-      req.logIn(user, (err) => {
-        if (err) {
-          return next(err);
-        }
-        // User has provided valid login credentials
-        return res.status(200).json({ message: "success" });
-      });
-    })(req, res, next);
-  });
+  /// AUTH ROUTES ///
 
-  // -------------------------------------------------------------------
-  // POST Route for signing up a user. Passwords will be auto hashed and stored securely thanks to
-  // how we configured our Sequelize User Model.
-  app.post("/api/signup", (req, res) => {
-    db.User.create({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      phone: req.body.phone,
-      password: req.body.password,
-    })
-      .then(() => {
-        // user is created successfully, proceed to log the user in
-        res.redirect(307, "/api/login");
-      })
-      .catch((error) => {
-        // otherwise send back an error
-        res.status(401).json(error);
-      });
-  });
+  // POST Route that signs a user in.
+  app.post("/api/login", authController.login);
 
-  // -------------------------------------------------------------------
-  // GET Route for logging user out
-  app.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/login");
-  });
+  // POST Route for signing up a user.
+  app.post("/api/signup", authController.signup);
 
-  // -------------------------------------------------------------------
-  // GET Route for getting some data about our user to be used by the client side
-  app.get("/api/user_data", (req, res) => {
-    if (!req.user) {
-      // The user is not logged in, send back an empty object
-      res.status(404).json({});
-    }
+  // GET Route for logging user out.
+  app.get("/logout", authController.logout);
 
-    // Otherwise send back the user's email and id
-    // Sending back a password, even a hashed password, isn't a good idea
-    db.User.findOne({
-      where: { id: req.user.id },
-      attributes: { exclude: ["password"] },
-    })
-      .then((dbUser) => {
-        res.status(200).json(dbUser);
-      })
-      .catch((error) => {
-        res.status(401).json(error);
-      });
-  });
+  // GET Route for getting data about a user to be used by the client side.
+  app.get("/api/user_data", authController.getUserData);
 
-  // -------------------------------------------------------------------
-  // PUT Route for updating the user's information.
-  app.put("/api/user_data", (req, res) => {
-    if (!req.user) {
-      return;
-    }
+  // PUT Route for updating a user's information.
+  app.put("/api/user_data", authController.updateUser);
 
-    db.User.update(
-      {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        phone: req.body.phone,
-      },
-      {
-        where: {
-          id: req.user.id,
-        },
-      }
-    )
-      .then((dbUser) => {
-        res.status(200).json(dbUser);
-      })
-      .catch((error) => {
-        res.status(401).json(error);
-      });
-  });
-
-  // -------------------------------------------------------------------
   // DELETE Route for deleting a user account.
-  app.delete("/api/user_data", (req, res) => {
-    db.User.destroy({
-      where: {
-        id: req.user.id,
-      },
-    })
-      .then((dbUser) => {
-        res.status(200).json(dbUser);
-      })
-      .catch((error) => {
-        res.status(401).json(error);
-      });
-  });
+  app.delete("/api/user_data", authController.deleteUser);
 };
