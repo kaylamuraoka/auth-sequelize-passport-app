7a71d63f0b5ef9331dedfe2738335bcbdb36bd04
kaylamuraoka <kmurs98@gmail.com>
2021-02-17 17:20:34 -1000

    updating config file to use local time zone

--- config/config.js
+++ config/config.js
@@ -7,6 +7,10 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: "mysql",
+    dialectOptions: {
+      useUTC: false, //for reading from database
+    },
+    timezone: "-10:00",
   },
   test: {
     username: "root",
