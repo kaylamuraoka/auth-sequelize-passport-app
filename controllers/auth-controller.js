6da5fc83e6e8cc885a1ec42c6b6c02eef4c9a5f7
kaylamuraoka <kmurs98@gmail.com>
2021-02-19 12:00:24 -1000

    reconfiguing sendResetLink() function in auth-controller.js to Configure MailGun with Nodemailer

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -4,6 +4,7 @@ const passport = require("../config/passport");
 const crypto = require("crypto");
 require("dotenv").config();
 const nodemailer = require("nodemailer");
+const nodemailMailgun = require("nodemailer-mailgun-transport");
 const bcrypt = require("bcryptjs");
 const { Op } = require("sequelize");
 
@@ -191,53 +192,40 @@ const authController = {
           resetPasswordExpires: Date.now() + 3600000, // valid for one hour after sending the link
         });
 
-        // Generate SMTP service account from ethereal.email
-        nodemailer.createTestAccount((err, account) => {
+        // Configure MailGun with Nodemailer
+        // Define authentication for nodemailMailgun
+        const auth = {
+          auth: {
+            api_key: process.env.MAILGUN_API_KEY,
+            domain: process.env.MAILGUN_DOMAIN,
+          },
+        };
+
+        // Create transporter - connecting nodemailer to mailGun
+        let transporter = nodemailer.createTransport(nodemailMailgun(auth));
+
+        // Define our mail options - what we want the user to see
+        const mailOptions = {
+          from: "User <app202055888@heroku.com",
+          to: `${user.email}`,
+          subject: "Password reset requested",
+          text:
+            `Hi ${user.fullName}\n\n` +
+            "You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n" +
+            "Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it \n\n" +
+            `https://fukunaga-and-associates.herokuapp.com/reset/${token}\n\n` +
+            "If you did not request this, please ignore this email and your password will remain unchanged.\n",
+        };
+
+        // Grab our transporter and use sendMail() function that contains the mailOptions
+        transporter.sendMail(mailOptions, function (err, info) {
           if (err) {
-            console.log("Failed to create a testing account. " + err.message);
+            console.log("Error occurred. " + err.message);
             return process.exit(1);
-          }
-
-          console.log("Credentials obtained, sending message...");
-
-          // Create a SMTP transporter object
-          let transporter = nodemailer.createTransport({
-            host: account.smtp.host,
-            port: account.smtp.port,
-            secure: account.smtp.secure,
-            auth: {
-              user: account.user,
-              pass: account.pass,
-            },
-          });
-
-          // Message object, creating the email template
-          let message = {
-            from: "sender@example.com",
-            to: `${user.email}`,
-            subject: "Password reset requested",
-            text:
-              `Hi ${user.fullName}\n\n` +
-              "You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n" +
-              "Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it \n\n" +
-              `http://localhost:8080/reset/${token}\n\n` +
-              "If you did not request this, please ignore this email and your password will remain unchanged.\n",
-          };
-
-          console.log("sending mail");
-
-          // Send Mail
-          transporter.sendMail(message, (err, info) => {
-            if (err) {
-              console.log("Error occurred. " + err.message);
-              return process.exit(1);
-            }
-
+          } else {
             console.log("Message sent: %s", info.messageId);
-            // Preview only available when sending through an Ethereal account
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
             res.status(200).json("recovery email sent");
-          });
+          }
         });
       }
     });
