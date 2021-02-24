9ee799a6c97b4fb00ea83b8b2df87ee550bffba4
kaylamuraoka <kmurs98@gmail.com>
2021-02-23 18:29:50 -1000

    updating coudinaryConfig.js file so that const cloudinary = require(cloudinary).v2

--- config/coudinaryConfig.js
+++ config/coudinaryConfig.js
@@ -1,10 +1,12 @@
 // Here we configure our cloudinary module using our configuration parameters
 require("dotenv").config();
-const cloudinary = require("cloudinary");
+const cloudinary = require("cloudinary").v2;
 
+// Write a cloudinary config
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
 module.exports = cloudinary;
